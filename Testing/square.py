8f46e31483769fc29b5d343783d5f8468e9d69d8
prasadraju2003 <g.leelasaiprasadraju@gami.com>
2022-11-03 20:47:42 +0530

    intro to testing

---
+++ Testing/square.py
@@ -0,0 +1,12 @@
+# square of a num
+def main():
+    x = int(input("what's x ?"))
+    print("x squared is",square(x))
+
+def square(n):
+    return n*n
+# This line use import these function without running main function
+if __name__=="__main__":
+    main()
+
+
